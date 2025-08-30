8528a544afbe025fe070577d69448d57d8b78972
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-08-30 15:24:31 +0300

    feat(middleware) run a branch of middleware when it is a sensitive request

--- Extainions/CustomMiddlewareExtensions.cs
+++ Extainions/CustomMiddlewareExtensions.cs
@@ -1,8 +1,13 @@
 public static class CustomMiddlewareExtensions
 {
-    
+
     public static IApplicationBuilder UseRequestResponseLogging(this IApplicationBuilder builder)
     {
         return builder.UseMiddleware<RequestResponseLoggingMiddleware>();
+    }
+
+    public static IApplicationBuilder UseAdminMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<AdminMiddleware>();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 
 using System.Threading.RateLimiting;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.RateLimiting;
 
@@ -16,7 +17,9 @@ builder.Services.AddRateLimiter(configureOptions =>
 
     configureOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests; // Set status code for rejected requests
 });
-
+//registers the core health check services with the application.
+//: Run your application and navigate to http://localhost:[port]/health. You should see a simple "Healthy" response in your browser.
+builder.Services.AddHealthChecks();
 var app = builder.Build();
 
 app.UseRateLimiter();
@@ -25,7 +28,26 @@ app.UseRequestResponseLogging();
 app.MapGet("/api/resources", () => "This api resource is rate limited")
 .RequireRateLimiting("fixed");
 
+app.MapHealthChecks("/health");
 
+//run a branch of middleware when it is a sensitive request
+app.UseWhen(
+    ctx => ctx.Request.Path.StartsWithSegments("/sensitive"),
+    branch =>
+    {
 
+        branch.UseMiddleware<RequestResponseLoggingMiddleware>();
+    }
+    );
 
+app.Map("/admin", adminApp =>
+{
+    adminApp.UseMiddleware<AdminMiddleware>();
+
+    // terminal middleware
+    adminApp.Run(async context =>
+    {
+        await context.Response.WriteAsync("Welcome to the admin area!");
+    });
+});
 app.Run();
