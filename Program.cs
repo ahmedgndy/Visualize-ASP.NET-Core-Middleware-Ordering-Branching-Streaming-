e3afb0eb7306da6141d392e8135db429da49778a
ahmedgndy <ahmed.mamdouh.elgndy@gmail.com>
2025-08-30 12:52:31 +0300

    feat(rate-limiter): add custom middleware to restrict sensitive pages to 2 requests per 10s

--- Program.cs
+++ Program.cs
@@ -1,6 +1,30 @@
+
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRateLimiter(configureOptions =>
+{
+    configureOptions.AddFixedWindowLimiter("fixed", RateLimiter =>
+       {
+           RateLimiter.PermitLimit = 2; // Allow 2 requests
+           RateLimiter.Window = TimeSpan.FromSeconds(10); // Within a 10-second window
+    
+           RateLimiter.QueueLimit = 0; // No queueing, extra requests are rejected immediately.
+       });
+
+    configureOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests; // Set status code for rejected requests
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRateLimiter();
+
+
+app.MapGet("/api/resources", () => "This api resource is rate limited")
+.RequireRateLimiting("fixed");
+
+
 
 app.Run();
